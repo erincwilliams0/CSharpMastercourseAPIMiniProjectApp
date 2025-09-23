2a9376a4a4eb97ce50536afd480304bb37d05cda
erin williams <erin.cwilliams0@gmail.com>
2025-09-23 16:30:52 -0500

    feat: Added Models for Person and Address

--- APIMiniProject/Models/AddressModel.cs
+++ APIMiniProject/Models/AddressModel.cs
@@ -2,5 +2,10 @@
 {
     public class AddressModel
     {
+        public int Id { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
     }
 }
--- APIMiniProject/Models/PersonModel.cs
+++ APIMiniProject/Models/PersonModel.cs
@@ -2,5 +2,9 @@
 {
     public class PersonModel
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
     }
 }
